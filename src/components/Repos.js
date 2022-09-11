cd398415709b42dc68e79834b6b24d866e05a9a8
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-09-11 18:41:06 -0500

    Repos Component

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
+  const { repos } = React.useContext(GithubContext);
+  console.log(repos);
+
   return <h2>repos component</h2>;
 };
 

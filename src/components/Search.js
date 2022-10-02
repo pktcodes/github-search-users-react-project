91f32ef8d001fb1dbabe83d2a6c4e7a78d24c288
Praveen Kumar <praveenkumarthipparam@gmail.com>
2022-10-02 00:14:05 -0500

    Error

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,7 @@ import { GithubContext } from "../context/context";
 
 const Search = () => {
   const [user, setUser] = React.useState("");
-  const { requests } = React.useContext(GithubContext);
+  const { requests, error } = React.useContext(GithubContext);
 
   const handleSubmit = (event) => {
     console.log(user);
@@ -21,6 +21,11 @@ const Search = () => {
   return (
     <section className="section">
       <Wrapper className="section-center">
+        {error.show && (
+          <ErrorWrapper>
+            <p>{error.msg}</p>
+          </ErrorWrapper>
+        )}
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <MdSearch></MdSearch>
--- src/context/context.js
+++ src/context/context.js
@@ -16,9 +16,15 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+
+  //error
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  //request loading
   const [requests, setRequests] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
 
+  //check rate
   const checkRequests = () => {
     axios(`${rootUrl}/rate_limit`)
       //axios gives promise so .then() and .catch() and response in json format
@@ -28,9 +34,13 @@ const GithubProvider = ({ children }) => {
         let {
           rate: { remaining },
         } = data;
+
+        // To check the error
+        // remaining = 0;
+
         setRequests(remaining);
         if (remaining === 0) {
-          //show an error
+          toggleError(true, "Sorry, you have exceeded your hourly rate limit!");
         }
       })
       .catch((error) => {
@@ -38,12 +48,17 @@ const GithubProvider = ({ children }) => {
       });
   };
 
-  useEffect(() => {
-    checkRequests();
-  }, []);
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
+
+  //Changed the anonymous function to function call to remove warning - Adding dependency
+  useEffect(checkRequests, []);
 
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
+    <GithubContext.Provider
+      value={{ githubUser, repos, followers, requests, error }}
+    >
       {children}
     </GithubContext.Provider>
   );
